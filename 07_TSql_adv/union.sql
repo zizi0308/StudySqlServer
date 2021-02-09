65f47900f8fec58da0dbd546d1f580617f8b217f
JO HUI JI <whgmlwl222@naver.com>
2021-02-09 17:27:23 +0900

    JOIN복습

---
+++ 07_TSql_adv/union.sql
@@ -0,0 +1,19 @@
+﻿-- union
+select clubName, buildingNum as bLocation from ClubTbl
+union
+select stdName, region from StdTbl;
+
+-- union all
+select stdName, region from StdTbl
+union all
+select stdName, region from StdTbl;
+
+-- except 2 번째 쿼리만 제외하고
+select clubName, buildingNum as bLocation from ClubTbl
+except
+select stdName, region from StdTbl;
+
+--intersect 2번째 쿼리만 해당하는 것만 조회
+select clubName, buildingNum as bLocation from ClubTbl
+intersect
+select stdName, region from StdTbl;
